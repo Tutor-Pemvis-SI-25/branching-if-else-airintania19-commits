c3b74925a5d205ea00bf60fd6d179f6a18a73c6f
airintania19-commits <airintania19@gmail.com>
2025-10-03 13:44:59 +0700

    PAIR 2

--- T00.java
+++ T00.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -31,7 +31,7 @@ public class JavaApplication {
         if (a % 2 == 0 && b % 2 == 0) {
             System.out.println("Hasil penjumlahan=" + a + b);
         } else {
-            if (a % 2 != 0 && b % 2 != 0) {
+            if (a % 2 == 1 && b % 2 == 1) {
                 System.out.println("Hasil perkalian=" + a * b);
             } else {
                 System.out.println("Berbeda jenis");
